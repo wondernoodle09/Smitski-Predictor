88a6423bc9bae2427771e7203436240cf21610ee
Annika <annikachavez44@gmail.com>
2026-05-21 22:25:49 -0700

    fixed failed to fetch

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
 from app.supabase_client import supabase
 
 app = FastAPI()
+
+# CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# Request body model
+class CollectionCreate(BaseModel):
+    name: str
 
 
 @app.get("/")
@@ -13,3 +33,12 @@ def root():
 def get_collections():
     response = supabase.table("collections").select("*").execute()
     return response.data
+
+
+@app.post("/collections")
+def create_collection(collection: CollectionCreate):
+    response = supabase.table("collections").insert({
+        "name": collection.name
+    }).execute()
+
+    return response.data[0]
